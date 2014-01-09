2908c8155495873c1df20881babf7e78b9fd93ad
Camilo Carrillo <ccarrillom@gmail.com>
2014-01-09 15:21:36 +0100

    adding more statistics for the test

---
+++ HiggsGenHltRecoAnalyzer/test/higgsgenhltreco.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+import FWCore.Utilities.FileUtils as FileUtils
+mylist = FileUtils.loadListFromFile('90.txt')
+process.source = cms.Source("PoolSource",fileNames =
+cms.untracked.vstring( *mylist ))
+
+process.demo = cms.EDAnalyzer('HiggsGenHltRecoAnalyzer',
+    GenParticleLabel = cms.InputTag("genParticles"),
+    hltInputTag      = cms.InputTag( "TriggerResults::HLT" )
+)
+
+
+process.p = cms.Path(process.demo)
